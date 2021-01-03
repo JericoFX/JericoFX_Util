997d755f0dbea88cb389e084ddba67a95e37ce16
JericoFX <56100605+JericoFX@users.noreply.github.com>
2021-01-03 18:23:23 -0300

    News Addon

--- clientfx.lua
+++ clientfx.lua
@@ -1,61 +1,172 @@
---# Print the information of a table to the console
---https://stackoverflow.com/questions/9168058/how-to-dump-a-table-to-console
+PlayerJob = {}
+FXCore = nil
 
-function tprint (tbl, indent)
-	if not indent then indent = 0 end
-	for k, v in pairs(tbl) do
-	  formatting = string.rep("  ", indent) .. k .. ": "
-	  if type(v) == "table" then
-		print(formatting)
-		tprint(v, indent+1)
-	  elseif type(v) == 'boolean' then
-		print(formatting .. tostring(v))      
-	  else
-		print(formatting .. v)
-	  end
-	end
-  end
-
---#######################################################
-
-DrawText3D = function(x,y,z, text, d)
-	coords = vector3(x,y,z)
-  
-	local camCoords = GetGameplayCamCoords()
-	local distance = #(coords - camCoords)
-  
-	if not size then size = 1 end
-	if not font then font = 1 end
-  
-	local dist = Vdist(GetEntityCoords(GetPlayerPed(-1)),coords)
-  
-	local scale = (size / distance) * 2
-	local fov = (1 / GetGameplayCamFov()) * 100
-	scale = scale * fov
-   
-	SetTextScale(0.0 * scale, 0.55 * scale)
-	SetTextFont(font)
-	SetTextColour(255, 255, 255, math.floor(math.max(0.1,255 / math.max(1.0, dist/(d / 10) )) ))
-	SetTextDropshadow(0, 0, 0, 0, 255)
-	SetTextDropShadow()
-	SetTextOutline()
-	SetTextCentre(true)
-  
-	SetDrawOrigin(coords, 0)
-	BeginTextCommandDisplayText('STRING')
-	AddTextComponentSubstringPlayerName(text)
-	EndTextCommandDisplayText(0.0, 0.0)
-	ClearDrawOrigin()
-  end
-
-  --#######################################################
-  --Distance between 2 points
-  vDist = function(v1,v2)  
-	if not v1 or not v2 or not v1.x or not v2.x or not v1.z or not v2.z then return 0; end
-	return math.sqrt( ((v1.x - v2.x)*(v1.x-v2.x)) + ((v1.y - v2.y)*(v1.y-v2.y)) + ((v1.z-v2.z)*(v1.z-v2.z)) ) 
+Citizen.CreateThread(function()
+	while FXCore == nil do
+		TriggerEvent('FXCore:GetObject', function(obj) FXCore = obj end)
+		Citizen.Wait(200)
   end
   
+end)
+RegisterNetEvent('FXCore:Client:OnPlayerLoaded')
+AddEventHandler('FXCore:Client:OnPlayerLoaded', function()
 
-  exports('vDist', function(...) vDist(...); end)
+    PlayerJob = FXCore.Functions.GetPlayerData().job
   
-  exports('tprint', function(...) tprint(...); end)
+
+    
+end)
+
+RegisterNetEvent('FXCore:Client:OnJobUpdate')
+AddEventHandler('FXCore:Client:OnJobUpdate', function(JobInfo)
+
+    PlayerJob = JobInfo
+
+end)
+
+
+function LocalInput(text, numeros, windoes) --SHOW ON SCREEN KEYBOARD FOR THE PRICE AND NAME
+    DisplayOnscreenKeyboard(1, text or "FMMC_MPM_NA", "", windoes or "", "", "", "", numeros or 30)
+    while (UpdateOnscreenKeyboard() == 0) do
+        DisableAllControlActions(0)
+        Wait(0)
+    end
+    if (GetOnscreenKeyboardResult()) then
+        local result = GetOnscreenKeyboardResult()
+        return result
+    end
+end
+function LocalInputInt(text, numeros, windoes) --SHOW ON SCREEN KEYBOARD FOR THE PRICE AND NAME BUT RETURN A NUMBER
+   DisplayOnscreenKeyboard(1, text or "FMMC_MPM_NA", "", windoes or "", "", "", "", numeros or 30)
+   while (UpdateOnscreenKeyboard() == 0) do
+       DisableAllControlActions(0)
+       Wait(0)
+   end
+   if (GetOnscreenKeyboardResult()) then
+       local result = GetOnscreenKeyboardResult()
+       return tonumber(result)
+   end
+end
+
+RegisterNetEvent('jerico:news')
+AddEventHandler('jerico:news', function()
+	--if  PlayerJob.name == "reporter" then
+	OpenMenu()
+--	end
+end)
+
+OpenMenu = function()
+
+	local assert = assert
+	local MenuV = assert(MenuV)
+	local menu = MenuV:CreateMenu("FX NEWS", 'News Menu', 'topleft', 255, 0, 0, 'size-125')
+	
+menu.Theme = "native"
+menu.Position = "topright"
+menu.Color = {R=255,G=255,B=255}
+		MenuV:OpenMenu(menu)
+		
+		local button = menu:AddButton({ icon = '😃', label = 'Post News', value = 10, description = 'Send the news to the phone' })	 
+		local button1 = menu:AddButton({ icon = '😃', label = 'Erase News', value = 10, description = 'Erase News' })	 
+		button:On("select",function()
+		
+			NuevasNoticias()
+		end)
+		button1:On("select",function()
+		
+		
+			BorrarNoticias()
+		end)
+	
+end
+
+--[[ Citizen.CreateThread(function()
+	 ]]
+--[[ end) ]]
+
+BorrarNoticias = function()
+
+ 	FXCore.Functions.TriggerCallback('crew-phone:get-news', function(news)
+        local elements = {}
+        for i=1, #news, 1 do
+            table.insert(elements, {label = json.decode(news[i].alldata).name, value = json.decode(news[i].id)})
+        end
+		local assert = assert
+		local MenuV = assert(MenuV)
+		local menu = MenuV:CreateMenu("FX NEWS", 'Delete Menu', 'topleft', 255, 0, 0, 'size-125')
+		MenuV:OpenMenu(menu)
+		menu.Theme = "native"
+		menu.Position = "topright"
+		for k,v in ipairs(elements) do
+			local button = menu:AddButton({ icon = '😃', label = v.label, value = v , description = v.label ,select = function(btn)
+				local value = btn.Value
+				menu.Title = "Delete News"..btn.Value.value
+				if value.value == tonumber(value.value) then
+					FXCore.Functions.Notify("News Deleted")
+					TriggerServerEvent("crew-phone:delete-news", value.value)
+				else
+					FXCore.Functions.Notify("News Not Deleted")
+				end
+
+			end})	  
+		end
+	
+	end)
+end
+
+
+NuevasNoticias = function()
+
+	local dialog
+	local dialog1
+	local dialog2
+	local dialog3
+	local dialog4
+	
+			 local assert = assert
+			  local MenuV = assert(MenuV)
+			  local menu = MenuV:CreateMenu("FX NEWS", 'News Menu', 'topleft', 255, 0, 0, 'size-125')
+			  MenuV:OpenMenu(menu)
+			  menu.Theme = "Native"
+			  local checkbox = menu:AddCheckbox({ icon = '💡', label = "Title", value = 'n' })
+			  local checkbox1 = menu:AddCheckbox({ icon = '💡', label = "Content", value = 'n', disabled = false })
+			  local checkbox2 = menu:AddCheckbox({ icon = '💡', label ="IMG", value = 'n' , disabled = false })
+			  local button = menu:AddButton({ icon = '😃', label = 'Send News', value = 10, description = 'Send the news to the phone' })	  
+			  checkbox:On("check",function()
+				  -- dialog1 = LocalInput("news",30, "Titulo")
+				  TriggerEvent("Input:Open","Title","FXCore",function(p)
+					local price = (p and tostring(p) and tostring(p) == "" )
+					
+						dialog1 = p
+					
+				  end)
+				   
+				   
+			  end) 
+			  checkbox1:On("check",function()
+				TriggerEvent("Input:Open","Contenido","FXCore",function(p)
+					local price = (p and tostring(p) and tostring(p) == "")
+					   
+						dialog2 = p
+				
+				end)
+				
+			
+			 end)
+			 checkbox2:On("check",function()
+				TriggerEvent("Input:Open","Contenido","FXCore",function(p)
+					local price = (p and tostring(p) and tostring(p) == "")
+				   
+						dialog3 = p
+						
+					
+					
+				end)
+			  end)
+			 
+			  button:On("select",function()
+				TriggerServerEvent("crew-phone:new-news", dialog1, dialog2, dialog3, "")
+			
+			end)
+
+end
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,15 @@
-fx_version 'adamant'
-
+fx_version 'cerulean'
 game 'gta5'
-
-description 'JericoFX Utils found on all over internet'
+lua54 'yes'
+name 'MenuV'
+version '1.5-beta'
+description 'FiveM menu library for creating menu\'s'
+author 'ThymonA'
+contact 'contact@arens.io'
+url 'https://github.com/ThymonA/menuv/'
 
 server_script 'serverfx.lua'
-client_script 'clientfx.lua'
+client_scripts {"@menuv/menuv.lua",
+    'clientfx.lua',
+    
+}
